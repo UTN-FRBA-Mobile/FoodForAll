f1b6959e26cf9256510c1403d0f110eb18d2d8b6
ignaciop000 <ignaciop000@gmail.com>
2025-09-09 12:08:00 -0300

    Initial scaffolding

---
+++ android/app/src/main/java/ar/edu/utn/frba/mobile/foodforall/MainActivity.kt
@@ -0,0 +1,47 @@
+package ar.edu.utn.frba.mobile.foodforall
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ar.edu.utn.frba.mobile.foodforall.ui.theme.FoodForAllTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FoodForAllTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FoodForAllTheme {
+        Greeting("Android")
+    }
+}
